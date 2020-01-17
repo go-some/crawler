ee9009ec93a971a79e52a908389dde42e257d0ff
dlwogns0128 <dlwogns0128@naver.com>
2020-01-17 13:53:03 +0900

    [ADD] coinmarketcap.com crawler
    - crawling Top 200 coins from coinmarketcap
    - crawl info: [name/symbol/price/volume/market cap]

---
+++ coinmarketcap.go
@@ -0,0 +1,43 @@
+
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+
+	fName := "cryptocoinmarketcap.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	writer.Write([]string{"Name", "Symbol", "Price (USD)", "Volume (USD)", "Market capacity (USD)"})
+
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	c.OnHTML(".cmc-table-row", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.ChildText("td.cmc-table__cell--sort-by__name"),
+			e.ChildText("td.cmc-table__cell--sort-by__symbol"),
+			e.ChildText("td.cmc-table__cell--sort-by__price"),
+			e.ChildText("td.cmc-table__cell--sort-by__volume-24-h"),
+			e.ChildText("td.cmc-table__cell--sort-by__market-cap"),
+		})
+
+	})
+
+	c.Visit("https://coinmarketcap.com/all/views/all/")
+
+	log.Printf("Scraping finished, check file %q for results\n", fName)
+}
