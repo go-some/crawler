9b654cba9fb870730c81d433aa57beadbbf0ed35
songjein <jeinsong100@gmail.com>
2020-01-27 11:27:47 +0000

    [UPDATE] mongodb writer implementation

--- README.md
+++ README.md
@@ -6,24 +6,34 @@ crowl 서비스의 크롤러를 구현합니다.
 ```bash
 go get -u github.com/gocolly/colly/...
 ```
+- 몽고디비를 위한 라이브러리를 설치합니다
+```bash
+go get go.mongodb.org/mongo-driver
+```
+- [mongodb-with-go-tutorial](https://www.mongodb.com/blog/post/mongodb-go-driver-tutorial)
+- DNS error 발생시 /etc/resolve.conf 수정 [참조](https://stackoverflow.com/questions/55660134/cant-connect-to-mongo-cloud-mongodb-database-in-golang-on-ubuntu)
+- [TODO] dep라고 하는 패키지 매니저가 있다고 합니다... 한 번 알아봐야 할 듯!
 
-## 설치
+## Installation 
 - go 설치 후
-- [work-space-path]/src/ 밑에 이 프로젝트를 clone 시킵니다.
-- 현재는 [work-space-path]/src/crawler 에 위치시켜놓고 쓰고있습니다.
-- 만약, [work-space-path]/src/github.com/go-some/crawler 와 같이 구성하실 경우엔 executor에서 import path를 적절히 수정해주세요.
+- [workspace-path] 밑에서 go-some crawler를 설치합니다
+```bash
+go get -u github.com/go-some/crawler
+```
+- .bashrc에 DBID, DBPW, DBADDR 환경변수를 정의합니다. (따로 문의)
 - 코드 수정 후 go install을 꼭 해주시고,
 - [executor(main.go)](https://github.com/go-some/executor)를 실행해 주세요.
 
-## 코드 설명
+## Structure
 - 각 크롤러 코드는 해당 사이트를 의미하는 struct를 정의하고,
 ```go
 type USToday struct {
 }
 ```
 - 특정 사이트를 크롤하는 Run 메서드를 구현 후, 리시버를 통해 struct와 매핑시킵니다.
+- Run 함수에서는 대상 문서 리스트를 크롤하고 DocsWriter를 통해 결과를 기록합니다.
 ```go
-func (rc *USToday) Run(wtr Writer) {
+func (rc *USToday) Run(wtr DocsWriter) {
   c := colly.NewCollector()
 
   docs := make([]News, 0, 100)
@@ -40,7 +50,7 @@ func (rc *USToday) Run(wtr Writer) {
   })
 
   c.OnScraped(func (r *colly.Response) {
-    wtr("ustoday.csv", docs)
+    wtr(docs)
   })
 
   c.Visit("https://www.usatoday.com/money/")
--- writer.go
+++ writer.go
@@ -1,13 +1,21 @@
 package crawler
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type DocsWriter interface {
 	WriteDocs([]News) (n int, err error)
 }
 
+/* define printerWriter ( */
 type printerWriter struct{}
 
 func (*printerWriter) WriteDocs(docs []News) (n int, err error) {
@@ -18,3 +26,57 @@ func (*printerWriter) WriteDocs(docs []News) (n int, err error) {
 func NewPrinterWriter() *printerWriter {
 	return &printerWriter{}
 }
+
+/* ) */
+
+/* define mongodbWriter ( */
+type mongoDBWriter struct{}
+
+func (*mongoDBWriter) WriteDocs(docs []News) (n int, err error) {
+	id, pw := os.Getenv("DBID"), os.Getenv("DBPW")
+	addrTemplate := os.Getenv("DBADDR")
+	fmt.Println(id, pw, addrTemplate)
+	mongoDBAddr := fmt.Sprintf(addrTemplate, id, pw)
+	fmt.Println("db addr", mongoDBAddr)
+	clientOptions := options.Client().ApplyURI(mongoDBAddr)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("test").Collection("news")
+
+	for _, doc := range docs {
+		insertResult, err := collection.InsertOne(context.TODO(), doc)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+	}
+
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+
+	return len(docs), nil
+}
+
+func NewMongoDBWriter() *mongoDBWriter {
+	return &mongoDBWriter{}
+}
+
+/* ) */
