d54ec9702d23b5dccc70319617834ae70927160b
songjein <jeinsong100@gmail.com>
2020-03-25 03:04:58 +0000

    [UPDATE] marketwatch

--- marketwatch.go
+++ marketwatch.go
@@ -1,7 +1,9 @@
 package crawler
 
 import (
+	"fmt"
 	"github.com/gocolly/colly"
+	"regexp"
 	"strings"
 )
 
@@ -9,25 +11,44 @@ type MarketWatch struct {
 }
 
 func (rc *MarketWatch) Run(wtr DocsWriter) {
-	c := colly.NewCollector()
+	rootCollector := colly.NewCollector(
+		colly.MaxDepth(3),
+		colly.URLFilters(
+			regexp.MustCompile("https://www\\.marketwatch\\.com/"),
+			regexp.MustCompile("https://www\\.marketwatch\\.com/story/.+"),
+		),
+		//colly.DisallowedURLFilters(
+		//	regexp.MustCompile("https://www\\.usatoday\\.com/opinion/"),
+		//),
+	)
+	rootCollector.AllowURLRevisit = false
 
-	docs := make([]News, 0, 100)
+	articleCollector := colly.NewCollector()
 
-	c.OnHTML("div.element--article", func(e *colly.HTMLElement) {
-		// site-specific patterns
-		title := strings.Trim(e.ChildText("h3.article__headline"), " ")
-		url := e.ChildAttr(".article__headline a", "href")
-		body := ""
-		time := e.ChildText(".article__timestamp")
-		origin := "https://www.marketwatch.com/"
-		// [TODO] add validation
-		doc := News{title, body, time, url, origin}
-		docs = append(docs, doc)
+	rootCollector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		if strings.Index(link, "marketwatch.com/story/") != -1 {
+			articleCollector.Visit(link)
+		} else {
+			e.Request.Visit(link)
+		}
 	})
 
-	c.OnScraped(func(r *colly.Response) {
-		wtr.WriteDocs(docs)
+	articleCollector.OnHTML(".region--primary", func(e *colly.HTMLElement) {
+		doc := News{
+			Title:  e.ChildText("h1.article__headline"),
+			Body:   e.ChildText("div.article__body "),
+			Time:   e.ChildText(".timestamp "),
+			Url:    e.Request.URL.String(),
+			Origin: "MarketWatch",
+		}
+		cnt, err := wtr.WriteDocs([]News{doc})
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(cnt, "docs saved")
+		}
 	})
 
-	c.Visit("https://www.marketwatch.com/investing/stocks?mod=exchange-traded-funds")
+	rootCollector.Visit("https://www.marketwatch.com/")
 }
