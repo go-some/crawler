3798cbd9f68d64d47063ba42715de0ceeee91665
songjein <jeinsong100@gmail.com>
2020-04-16 16:14:37 +0000

    [MOD] add fields for NER&Summarization

--- news.go
+++ news.go
@@ -1,6 +1,15 @@
 package crawler
 
+import (
+	"github.com/go-some/txtanalyzer"
+)
+
 type News struct {
 	Title, Body, Url, Time, Origin, ImgUrl string
+	BodySum                                string
 	HasGraphImg                            bool
+	EntitiesInTitle                        []txtanalyzer.Entity
+	PersonList                             []string
+	OrgList                                []string
+	ProdList                               []string
 }
